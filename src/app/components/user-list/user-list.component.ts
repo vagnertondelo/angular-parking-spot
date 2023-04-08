01b92da218e1d610ea130fd866c97d04e0a1fd4f
Vagner K Tondello <vagnertondello@gmail.com>
2023-04-08 15:17:07 -0300

    first commit

---
+++ src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
+
+}
